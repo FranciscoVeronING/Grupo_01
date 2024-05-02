8f0cc43192d7047b4264e502ae4560582f3b8caf
DarioMayor <dario98mayor@gmail.com>
2024-05-02 11:03:45 -0300

    Agregados y anotaciones.

--- src/main/java/models/Cliente.java
+++ src/main/java/models/Cliente.java
@@ -13,7 +13,7 @@ import java.util.GregorianCalendar;
  *
  *
  */
-public class Cliente {
+public class Cliente extends Usuario{
     private String nombre;
     private String apellido;
     private String contrasenia;
